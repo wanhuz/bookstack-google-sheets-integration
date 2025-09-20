335ae6876ae7a27f065a16deadf5b36de363a8a5
wan <wanhuz96@gmail.com>
2025-09-20 20:42:34 +0800

    Update sheet-toggle.js

--- sheet-toggle.js
+++ sheet-toggle.js
@@ -31,6 +31,12 @@ document.addEventListener("DOMContentLoaded", () => {
     </svg>
   `;
 
+  const gearSVG = `
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true">
+            <path fill="#fff" d="M19.14 12.94c.04-.31.06-.63.06-.94s-.02-.63-.06-.94l2.03-1.58c.18-.14.23-.41.12-.61l-1.92-3.32c-.12-.21-.37-.3-.59-.22l-2.39.96c-.5-.38-1.02-.7-1.6-.94L14.5 2.81c-.03-.23-.25-.41-.5-.41h-4c-.25 0-.47.18-.5.41L8.34 5.02c-.57.24-1.09.56-1.59.94L4.36 5c-.22-.08-.47.01-.59.22L1.85 8.55c-.11.21-.06.48.12.61l2.02 1.58c-.04.31-.06.63-.06.94s.02.63.06.94L1.94 14.95c-.18.14-.23.41-.12.61l1.92 3.32c.12.21.37.3.59.22l2.39-.96c.5.38 1.02.7 1.6.94l.67 2.21c.03.23.25.41.5.41h4c.25 0 .47-.18.5-.41l.67-2.21c.58-.24 1.1-.56 1.6-.94l2.39.96c.22.08.47-.01.59-.22l1.92-3.32c.12-.21.07-.48-.12-.61l-2.03-1.58zM12 15.5a3.5 3.5 0 1 1 0-7 3.5 3.5 0 0 1 0 7z"/>
+    </svg>
+ `
+
   // --- Create toggle button ---
   const sheetBtn = document.createElement("button");
   sheetBtn.id = "sheet-toggle-btn";
@@ -50,12 +56,7 @@ document.addEventListener("DOMContentLoaded", () => {
   editBtn.id = "floating-edit-btn";
   editBtn.classList.add("floating-btn");
   editBtn.title = "Edit Page";
-  editBtn.innerHTML = `
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-      <path d="M12 20h9"/>
-      <path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"/>
-    </svg>
-  `;
+  editBtn.innerHTML = gearSVG;
   editBtn.onclick = () => window.location.href = window.location.href + "/edit";
 
   // --- Append button to page ---
