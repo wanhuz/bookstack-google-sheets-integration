b2e4c352143773fc4558f409493ed92034ccdde3
wan <wanhuz96@gmail.com>
2025-09-20 20:31:23 +0800

    Fix button not in place

--- sheet-toggle.js
+++ sheet-toggle.js
@@ -44,9 +44,24 @@ document.addEventListener("DOMContentLoaded", () => {
     updateTitleBar();
   };
 
+  //   Create page editing button
+
+  const editBtn = document.createElement("button");
+  editBtn.id = "floating-edit-btn";
+  editBtn.classList.add("floating-btn");
+  editBtn.title = "Edit Page";
+  editBtn.innerHTML = `
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      <path d="M12 20h9"/>
+      <path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"/>
+    </svg>
+  `;
+  editBtn.onclick = () => window.location.href = window.location.href + "/edit";
+
   // --- Append button to page ---
   const btnWrapper = document.createElement("div");
   btnWrapper.id = "floating-btns";
   btnWrapper.appendChild(sheetBtn);
+  btnWrapper.appendChild(editBtn);
   document.body.appendChild(btnWrapper);
 });
