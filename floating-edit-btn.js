fd717a1b10c9290282d2037d4c8c68aae18404c6
wan <wanhuz96@gmail.com>
2025-09-20 20:24:28 +0800

    Update floating-edit-btn.js

--- floating-edit-btn.js
+++ floating-edit-btn.js
@@ -6,17 +6,19 @@ document.addEventListener("DOMContentLoaded", () => {
   // Enable sheet fullscreen
   document.body.classList.add("sheet-fullscreen");
 
-  const editBtn = document.createElement("button");
-  editBtn.id = "floating-edit-btn";
-  editBtn.classList.add("floating-btn");
-  editBtn.title = "Edit Page";
-  editBtn.innerHTML = `
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-      <path d="M12 20h9"/>
-      <path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"/>
-    </svg>
-  `;
-  editBtn.onclick = () => window.location.href = window.location.href + "/edit";
+    const editBtn = document.createElement("button");
+    editBtn.id = "floating-edit-btn";
+    editBtn.classList.add("floating-btn");
+    editBtn.title = "Edit Page";
+    editBtn.innerHTML = `<svg ...></svg>`;
 
-  document.body.appendChild(editBtn);
+    // Append to shared wrapper
+    let btnWrapper = document.querySelector("#floating-btns");
+    if (!btnWrapper) {
+        btnWrapper = document.createElement("div");
+        btnWrapper.id = "floating-btns";
+        document.body.appendChild(btnWrapper);
+    }
+    btnWrapper.appendChild(editBtn);
+
 });
